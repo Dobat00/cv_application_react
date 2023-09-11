bfc5201b7d2ee2e3f5c179ee89363ce269747945
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-09-11 17:51:02 -0300

    salvando informacoes pessoais em um objeto

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,14 +1,45 @@
 import "./App.css";
-import Button from "./components/Button";
-import Forms from "./components/Forms";
+import { useState } from "react";
 
 function App() {
-  return(
+  return (
     <div className="content">
-      <Forms valor1={'nome'} valor2={'email'} valor3={'telefone'}></Forms>
+      <Forms />
     </div>
-  )
+  );
 }
 
+function Forms() {
+  const [pessoal, setPessoal] = useState({ nome: "", email: "", telefone: "" });
+  function handleSubmit() {
+    let nome1 = document.getElementById("inputNome").value;
+    let email1 = document.getElementById("inputEmail").value;
+    let telefone1 = document.getElementById("inputTelefone").value;
+    const novoPessoal = { nome: nome1, email: email1, telefone: telefone1 };
+    setPessoal(novoPessoal);
+    console.log(pessoal);
+  }
+  return (
+    <div>
+      <label>nome</label>
+      <input type="text" id="inputNome"></input>
+      <br></br>
+      <label>email</label>
+      <input type="text" id="inputEmail"></input>
+      <br></br>
 
+      <label>telefone</label>
+      <input type="text" id="inputTelefone"></input>
+      <Button text="submit" onClick={handleSubmit} />
+    </div>
+  );
+}
+
+function Button({ text, onClick }) {
+  return (
+    <div>
+      <button onClick={onClick}>{text}</button>
+    </div>
+  );
+}
 export default App;
--- cv-app/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import Forms from "./Forms";
-
-function Button({texto, func}) {
-  return <button onClick={func}>{texto}</button>;
-}
-
-
-export default Button;
